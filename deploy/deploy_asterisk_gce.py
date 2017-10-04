d86bfb06fb770b0a324fa7692ce86409d4927772
Or Polaczek <or.polaczek@forter.com>
2017-10-04 05:14:45 +0300

    With multiregion trick

--- deploy/deploy_asterisk_gce.py
+++ deploy/deploy_asterisk_gce.py
@@ -3,23 +3,28 @@ from libcloud.compute.types import Provider
 from libcloud.compute.providers import get_driver
 from pprint import pprint
 
+from GCECreds import GCECreds
+
+deployment_name = "astricon-{}".format(datetime.datetime.now().strftime('%Y-%m-%dt%H%M%S'))
+print("Starting Asterisk deployment: {}".format(deployment_name))
+
 ComputeEngine = get_driver(Provider.GCE)
 
-driver = ComputeEngine(
+driver = ComputeEngine(GCECreds.SERVICE_ACCOUNT,
+                       GCECreds.KEY_FILE,
                        datacenter='us-central1-a',
                        project='astricon-2017-demos')
 
 images = driver.list_images()
 sizes = driver.list_sizes()
 
+# pprint(images)
 IMAGE_ID = '4257209330239857983' # CentOS 6
 image = [i for i in images if i.id == IMAGE_ID][0]
 
-pprint(images)
-
 # deploy_node takes the same base keyword arguments as create_node.
 node = driver.deploy_node(
-    "astricon-{}".format(datetime.datetime.now().strftime('%Y-%m-%dt%H%M%S')),
+    deployment_name,
     sizes[0],
     image,
     'deploy-script.sh',
@@ -28,3 +33,6 @@ node = driver.deploy_node(
 print("Waiting for Node")
 driver.wait_until_running([node], 10, 1000)
 print("Node is now running")
+print("")
+print("Your new machine is running: {}".format(deployment_name))
+print("Visit: https://console.cloud.google.com/compute/instances?project=astricon-2017-demos")
--- deploy/deploy_dockerized_asterisk_gce.py
+++ deploy/deploy_dockerized_asterisk_gce.py
@@ -3,29 +3,45 @@ from libcloud.compute.types import Provider
 from libcloud.compute.providers import get_driver
 from pprint import pprint
 
-ComputeEngine = get_driver(Provider.GCE)
+from GCECreds import GCECreds
 
-driver = ComputeEngine('deployer@astricon-2017-demos.iam.gserviceaccount.com',
-                       'astricon-2017-demos-e03de3565b4d.json',
-                       datacenter='us-central1-a',
-                       project='astricon-2017-demos')
+def gce_driver_by_region(region):
+    ComputeEngine = get_driver(Provider.GCE)
+    driver = ComputeEngine(GCECreds.SERVICE_ACCOUNT,
+                           GCECreds.KEY_FILE,
+                           datacenter=region,
+                           project='astricon-2017-demos')
+    return driver
 
-images = driver.list_images()
-sizes = driver.list_sizes()
+def deploy_node(driver, deployment_name):
+    images = driver.list_images()
+    sizes = driver.list_sizes()
 
-IMAGE_ID = '8193040493265699771' # CentOS 7
-image = [i for i in images if i.id == IMAGE_ID][0]
+    # pprint(images)
+    IMAGE_ID = '8193040493265699771' # CentOS 7
+    image = [i for i in images if i.id == IMAGE_ID][0]
 
-pprint(images)
+    # deploy_node takes the same base keyword arguments as create_node.
+    node = driver.deploy_node(
+        deployment_name,
+        sizes[0],
+        image,
+        'deploy-dockerized-asterisk.sh',
+        ex_network='default')
+    return node
 
-# deploy_node takes the same base keyword arguments as create_node.
-node = driver.deploy_node(
-    "astricon-dkr-{}".format(datetime.datetime.now().strftime('%Y-%m-%dt%H%M%S')),
-    sizes[0],
-    image,
-    'deploy-dockerized-asterisk.sh',
-    ex_network='default')
+deployment_name = "astricon-dkr-{}".format(datetime.datetime.now().strftime('%Y-%m-%dt%H%M%S'))
+print("Starting Asterisk deployment: {}".format(deployment_name))
 
-print("Waiting for Node")
-driver.wait_until_running([node], 10, 1000)
-print("Node is now running")
+REGIONS_TO_DEPLOY_MACHINES_AT = ['us-central1-a']
+
+for region in REGIONS_TO_DEPLOY_MACHINES_AT:
+    print("Deploying {} in region {}".format(deployment_name, region))
+    driver = gce_driver_by_region('us-central1-a')
+    node = deploy_node(driver, deployment_name)
+    print("Waiting for Node")
+    driver.wait_until_running([node], 10, 1000)
+    print("Node is now running")
+    print("")
+    print("Your new machine is running: {}".format(deployment_name))
+    print("Visit: https://console.cloud.google.com/compute/instances?project=astricon-2017-demos")
